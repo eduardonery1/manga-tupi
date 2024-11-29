611a9fb0bcba43417fbb1a62bb62806912cf93f8
Eduardo Nery <eduardo.nery8@gmail.com>
2024-11-28 22:00:03 -0300

    better chapter selection

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from sys import exit
 from menu import menu
 from pathlib import Path
-
+from collections import defaultdict
 
 def run(dir_path):
     subprocess.run(["feh", dir_path, "-R 1", "--fullscreen", "--scale-down"])
@@ -25,25 +25,25 @@ if __name__=="__main__":
     titles = [manga["attributes"]["title"]["en"] if "pt-br" not in manga["attributes"]["altTitles"] else manga["attributes"]["altTitles"]["pt-br"] for manga in r.json()["data"]]
     title_ids = [manga["id"] for manga in r.json()["data"]] 
     selected_title = menu(titles)
-    if selected_title == "EXIT":
-        exit()
 
-    chapters = requests.get(
-        f"{base_url}/manga/{title_ids[titles.index(selected_title)]}/feed?includeFuturePublishAt=0&includeExternalUrl=0&includeEmptyPages=0&limit=500&translatedLanguage[]=en&translatedLanguage[]=pt-br"
-    )
+
+    query = "includeFuturePublishAt=0&includeExternalUrl=0&includeEmptyPages=0&limit=500&translatedLanguage[]=en&translatedLanguage[]=pt-br"
+    chapters = requests.get(f"{base_url}/manga/{title_ids[titles.index(selected_title)]}/feed?{query}")
     #print(chapters.text)
     chapters = chapters.json()["data"]
-    chapters = [chaps for chaps in filter(lambda chap: chap["attributes"]["translatedLanguage"] == "pt-br" or chap["attributes"]["translatedLanguage"] == "en", chapters)]
-    chapters.sort(key = lambda chap: float(chap["attributes"]["chapter"]))
-    chapters_num = [chap["attributes"]["chapter"] + " - " + chap["attributes"]["translatedLanguage"] for chap in chapters]
-    chapter_ids = [chap["id"] for chap in chapters]
-    selected_chapter = menu(chapters_num)
-    if selected_chapter == "EXIT":
-        exit()
+    
+    chapter_sources = defaultdict(list)
+    for chap in chapters:
+        chapter_sources[chap["attributes"]["chapter"]].append(chap)
 
-    pages = requests.get(
-        f"{base_url}/at-home/server/{chapter_ids[chapters_num.index(selected_chapter)]}"
-    ).json()
+    chapters_num = [f"{chap:.0f}" if chap == int(chap) else f"{chap:.1f}" for chap in sorted(list(map(float, chapter_sources.keys())))]
+    selected_chapter = menu(chapters_num)
+
+    chapter_translates = [chap["attributes"]["translatedLanguage"] + " " + str(i + 1) for i, chap in enumerate(chapter_sources[selected_chapter])]
+    chapter_ids = [chap["id"] for chap in chapter_sources[selected_chapter]]
+    selected_opt = menu(chapter_translates)
+
+    pages = requests.get(f"{base_url}/at-home/server/{chapter_ids[chapter_translates.index(selected_opt)]}").json()
     dir_path = Path(f"{selected_title}/{selected_chapter}")
     dir_path.mkdir(parents=True, exist_ok=True)
     first = True
--- menu.py
+++ menu.py
@@ -1,57 +1,64 @@
 import curses
+from sys import exit
 
 
-def __menu(stdscr, menu, result) -> str:
-    menu.append("EXIT")
-    # Clear the screen
+def __menu(stdscr, menu, msg, result) -> str:
+    menu.append("Sair")
     stdscr.clear()
-    curses.curs_set(0)  # Hide the cursor
-    
-    # Initialize colors
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_YELLOW)  # Black text on yellow background
+    curses.curs_set(0)  
 
-    # Define menu 
-    current_option = 0
     screen_height, screen_width = stdscr.getmaxyx()
     display_height = screen_height - 2  # Leave space for padding at the top and bottom
+
+    curses.start_color()
+
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_YELLOW)  # Black text on yellow background
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+
+
+    current_option = 0
     start_index = 0  # Index of the first displayed option
 
     while True:
         stdscr.clear()
+        
+        stdscr.attron(curses.color_pair(2))
+        stdscr.addstr(1, screen_width//5 - len(msg)//2, msg.upper())
+        stdscr.attroff(curses.color_pair(2))
+    
         end_index = start_index + display_height
         visible_options = menu[start_index:end_index] 
         # Display menu
         for idx, row in enumerate(visible_options):
             if start_index + idx == current_option:
                 stdscr.attron(curses.color_pair(1))  # Highlight current option
-                stdscr.addstr(idx + 1, 2, row)
+                stdscr.addstr(idx + 2, 2, row)
                 stdscr.attroff(curses.color_pair(1))
             else:
-                stdscr.addstr(idx + 1, 2, row)
-
-        # Get user input
+                stdscr.addstr(idx + 2, 2, row)
         key = stdscr.getch()
 
         # Arrow key navigation
         if key == curses.KEY_UP:
             current_option = (current_option - 1) % len(menu)
-            if current_option < start_index or current_option == len(menu) - 1:
+            if current_option < start_index:
                 start_index = current_option
+            elif current_option == len(menu) - 1 and display_height < len(menu):
+                    start_index = current_option - display_height + 1
+
         elif key == curses.KEY_DOWN:
             current_option = (current_option + 1) % len(menu)
             if current_option >= end_index or current_option == 0:
                 start_index = current_option
         elif key == curses.KEY_ENTER or key in [10, 13]:
-            #stdscr.addstr(len(menu) + 3, 2, f"You selected '{menu[current_option]}'")
-            #stdscr.refresh()
-            #stdscr.getch()  # Wait for another key press
             result.append(menu[current_option])
             break
 
-def menu(opts: list[str]) -> str:
+def menu(opts: list[str], msg="") -> str:
     selected = [] 
-    curses.wrapper(lambda stdscr: __menu(stdscr, opts, result=selected))
+    curses.wrapper(lambda stdscr: __menu(stdscr, opts, msg, result=selected))
+    if selected[0] == "Sair":
+        exit()
     return selected[0]
 
 if __name__ == "__main__":
